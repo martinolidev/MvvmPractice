fb8276d23e820071618895488e89a373d89569cf
Martin Martinez <mtz.martin@proton.me>
2023-11-16 23:15:19 -0600

    Initial Commit

---
+++ MvvmPractice/MvvmPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmPracticeApp.swift
+//  MvvmPractice
+//
+//  Created by Martin Martinez on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MvvmPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
